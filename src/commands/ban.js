cae5135b0d4b8f1fa3951fc522609155c7c3b3da
Vinicius Prado <vinicavagna@gmail.com>
2021-04-08 18:48:54 -0300

    Create ban command

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -2,23 +2,23 @@ const { getUserFromMention } = require('../util/getUser')
 
 module.exports = {
     validate(client, message, args){
-        if(!message.member.hasPermission(['ban_MEMBERS'])){
+        if(!message.member.hasPermission(['KICK_MEMBERS'])){
             throw new Error('no_permissions');
         }
         if(!args[0]){
-            message.reply('Lembre-se de específicar o usuário a ser banado');
+            message.reply('Lembre-se de específicar o usuário a ser Kickado');
             throw new Error('invalid_syntax');
         }
     },
     async run(client, message, args){
-        let banMsg = `Os seguintes usuários foram banidos:\n`;
+        let kickMsg = `Os seguintes usuários foram kickados:\n`;
         args.map(async (value, index) =>{
             let member = getUserFromMention(value, client);
-            banMsg+= `-> ${member.username}\n`;
+            kickMsg+= `-> ${member.username}\n`;
             
-            await message.guild.member(member).ban();
+            await message.guild.member(member).kick();
         });
-        return message.channel.send(banMsg);
+        return message.channel.send(kickMsg);
     },
     get command(){
         return {
