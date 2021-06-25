f7cfb08260b0ffab91fc76ce65348161ef600739
Vinicius Prado <vinicavagna@gmail.com>
2021-06-25 12:01:31 -0300

    :sparkles: added leave command

--- src/commands/leave.js
+++ src/commands/leave.js
@@ -1,35 +1,38 @@
 const categories = require('../usersCategory');
+require('./join');
 
 module.exports = {
   validate(client, message, args) {
     const voiceChannel = message.member.voice.channel;
+
+    const inSameChannel = client.voice.connections.some(
+      (connection) => connection.channel.id === message.member.voice.channelID,
+    );
+
     if (!voiceChannel) {
       message.reply('Você precisa estar conectado a um canal de voz né amigão');
-
       throw new Error('user_voice_channel_not_found');
     }
 
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-
-    if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
-      message.reply('Preciso de permissões para falar nesse canal');
-      throw new Error('no_permissions');
+    if (!inSameChannel) {
+      message.reply('Tem que estar no mesmo canal de voz que eu né amigão');
+      throw new Error('not_in_same_channel');
     }
   },
   async run(client, message, args) {
     const voiceChannel = message.member.voice.channel;
 
     try {
-      var connection = await voiceChannel.join();
+      connection = await voiceChannel.leave();
     } catch (err) {
-      message.channel.send('Não foi possível se conectar no canal');
+      message.channel.send('Não foi possível se desconectar no canal');
       throw new Error(err);
     }
   },
   command: {
-    name: 'join',
+    name: 'leave',
     category: categories.USER,
-    description: 'Entra no seu canal de voz',
+    description: 'Sai do seu canal de voz',
     usage: 'Usuários',
   },
 };
